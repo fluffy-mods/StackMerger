0214ca29014bdc53c6fd52fc72337fabf9d35120
Fluffy <fluffy.l2032@gmail.com>
2017-11-19 12:47:55 +0100

    add logger utils [nolog]

---
+++ Source/StackMerger/Logger.cs
@@ -0,0 +1,39 @@
+﻿// Logger.cs
+// Copyright Karel Kroeze, 2017-2017
+    using System.Diagnostics;
+    using Verse;
+
+namespace StackMerger
+{
+    public static class Logger
+    {
+        public static string Identifier => "WorkTab";
+
+        public static string FormatMessage( string msg )
+        {
+            return Identifier + " :: " + msg;
+        }
+
+        [Conditional( "DEBUG" )]
+        public static void Debug( string msg )
+        {
+            Log.Message( FormatMessage( msg ) );
+        }
+
+        [Conditional( "TRACE" )]
+        public static void Trace( string msg )
+        {
+            Log.Error( FormatMessage( msg ) );
+        }
+
+        public static void Error( string msg )
+        {
+            Log.Error( FormatMessage( msg ) );
+        }
+
+        public static void Message( string msg )
+        {
+            Log.Message( FormatMessage( msg ) );
+        }
+    }
+}
